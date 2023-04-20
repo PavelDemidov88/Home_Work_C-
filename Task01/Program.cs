610d6b4065814f63467168fe6f9bb8045ef2cccf
Pavel <alopavlo@mail.ru>
2023-04-20 21:32:48 +0300

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат(число умноженное само на себя)
+
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Квадрат нашего числа = " + number * number);
