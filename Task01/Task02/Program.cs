1323788034192254b3ec1c2e9220e08f62185d96
Pavel <alopavlo@mail.ru>
2023-04-20 22:08:09 +0300

    Task02

---
+++ Task01/Task02/Program.cs
@@ -0,0 +1,10 @@
+﻿// Ввести два числа и определить не является ли второе квадратом первого
+
+int a=int.Parse(Console.ReadLine());
+int b=int.Parse(Console.ReadLine());
+if (b*b == a)
+   Console.WriteLine("ДА");   
+else
+{
+  Console.WriteLine("НЕТ");   
+}
